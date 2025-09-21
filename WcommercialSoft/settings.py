6fbb32b90fb3107dec63e8e076f17fe6b9357346
SALIOU MANSARE <120379399+laguisoft@users.noreply.github.com>
2025-09-21 05:14:23 +0000

    deployement djimafa

--- WcommercialSoft/settings.py
+++ WcommercialSoft/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'WcommercialSoft.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 """
 # client gberedoudolar (commercialSoft)
@@ -188,6 +188,29 @@ DATABASES = {
     }
 }
 """
+
+if os.getenv("DJANGO_ENV") == "production":
+    # Base en ligne (MySQL sur PythonAnywhere)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': '3306',
+        }
+    }
+else:
+    # Base locale (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
