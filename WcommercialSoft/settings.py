f213c1b62bec774f73a98adaaed85d46127cad84
SALIOU MANSARE <120379399+laguisoft@users.noreply.github.com>
2025-09-30 20:04:34 +0000

    vente hors ligne

--- WcommercialSoft/settings.py
+++ WcommercialSoft/settings.py
@@ -174,7 +174,7 @@ DATABASES = {
         'PORT': '3306',  # Port MySQL
     }
 }
-
+"""
 
 
 DATABASES = {
@@ -210,7 +210,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
