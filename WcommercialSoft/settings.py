02e4aa7641c8a08c841ce498dbdd42ef116c4ad9
SALIOU MANSARE <salioumansare9@gmail.com>
2026-04-23 00:27:49 +0000

    correction

--- WcommercialSoft/settings.py
+++ WcommercialSoft/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'WcommercialSoft.wsgi.application'
 
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 
 
@@ -333,7 +333,7 @@ DATABASES = {
 
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -344,7 +344,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
